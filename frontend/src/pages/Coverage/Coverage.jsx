b41aa6d2bd738c8017e50a4397afe7ed5f4dfed3
adrian4058 <adrian_2016_@outlook.es>
2024-10-21 22:09:21 -0300

    update map

--- frontend/src/pages/Coverage/Coverage.jsx
+++ frontend/src/pages/Coverage/Coverage.jsx
@@ -10,25 +10,11 @@ const Coverage = ({ currentPath }) => {
       <Header currentPath={currentPath} />
       <div className="w-max py-5 coverage-desktop">
         <div className="py-4">
-          <h1 className="fw-bold">Zona de Distribuición</h1>
+          <h1 className="fw-bold">Zona de Cobertura</h1>
           <p>
             Nuestra cobertura se basa en las provincias de Santa Fe y Entre Ríos, atendiendo de forma independiente las
             principales ciudades y localidades de ambas regiones.
           </p>
-          <div>
-            <h3 className="fw-bold pb-2">Santa Fe:</h3>
-            <ul>
-              <li>+ de 135 Localidades</li>
-              <li>+ de 6.200 Puntos de Venta</li>
-            </ul>
-          </div>
-          <div>
-            <h3 className="fw-bold pb-2">Entre Ríos:</h3>
-            <ul>
-              <li>+ de 70 Localidades</li>
-              <li>+ de 3.400 Puntos de Venta</li>
-            </ul>
-          </div>
         </div>
         <img className="img-cobertura" src={imgCobertura} />
       </div>
